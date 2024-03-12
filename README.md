49f4aff95b05c6ec432ff6a4411c7bbbfa67e66d
LihMartins <lihmmartins@gmail.com>
2024-03-12 22:22:07 +0000

    Update Document Files

---
+++ README.md
@@ -0,0 +1,158 @@
+# Final Fantasy Memory-Card Game
+
+Welcome to the Final Fantasy Memory-Card game project. This project was initially created using Visual Studio Code.
+
+## Project Overview
+
+This project is a combination of my passion for the Final Fantasy franchise and my love for puzzles. It's a mini-game designed to challenge your memory and keep your brain active.
+
+## Layout Design
+
+The game starts with a container box that serves two purposes:
+
+1. Explaining the rules of the game.
+2. Requesting the user to input their name in a form.
+
+Once the user inputs their name, if they win the game, it will be displayed on the ScoreBoard at the end.
+
+### Main Page Layout
+
+![Main Page Layout](../finalfantasy/validator-images/Main%20Page.PNG)
+
+### Running Out of Time Layout
+
+![Running Out of Time Layout](../finalfantasy/validator-images/Runningtime.PNG)
+
+### Winners Layout
+
+![Winners Layout](../finalfantasy/validator-images/winner.PNG)
+
+### Loser Layout
+
+![Game Over Layout](../finalfantasy/validator-images/gameover.PNG)
+
+## Game Rules
+
+The game begins as soon as the player hits the start button. A timer is set and the countdown begins. The objective of the game is to match all cards before the timer runs out. If the time is over and the player has not matched all cards, it's a Game Over.
+
+## Features
+
+The main page of the game features a Start Up form that explains the rules of the game and sets the goal.
+
+Players are required to input their name in a provided field. This name will be displayed on the scoreboard throughout the game.
+
+To win, players must match all cards within a 90-second timeframe. Their name will be displayed on the scoreboard, where they can strive to beat other players or improve their own best performance.
+
+If a player is unable to match all cards within the 90-second timeframe, it's game over.
+
+Each new round features a random shuffle of the cards.
+
+The background of the Start-up field features an image from Final Fantasy. Once the start button is pressed, the cards are displayed back-faced, showing another Final Fantasy image, and the countdown timer starts. That's when the adventure begins - 90 seconds to find and match all Final Fantasy themed images!
+
+The chosen fonts for the game are Kavon, [from Sorkintype](https://www.sorkintype.com), and 'Courier New', Courier, monospace.
+
+The game also features sound effects for various events such as flipping the cards, matching cards, running out of time, and winning/losing the game. The Flip, Unflip, and Success sounds were imported from [@vsafonova's GitHub](https://github.com/vsafonova/memory-card-game). The [Winning Sound](https://soundboardguy.com/sounds/final-fantasy-7-victory-theme-mp3/#google_vignette) and [Game Over](https://pixabay.com/sound-effects/search/game-over/) sound were sourced from external websites.
+
+## Unfixed Bugs
+
+No unfixed bugs (or any bug) were found in the game.
+
+## Deployment
+
+Deploying Indieverse to GitHub was a challenging yet rewarding journey. As a first-time GitHub user, I encountered some hurdles, but the learning process was invaluable.
+
+### Steps to Deploy
+
+1. **Linking Visual Studio Code to GitHub:**
+   - To initiate the deployment process, I first linked my Visual Studio Code directory to GitHub.
+   - Created a repository on GitHub to host the Indieverse project.
+
+2. **Adjusting Branch for Deployment:**
+   - As part of the deployment setup, I had to adjust the branch settings.
+   - Followed the steps provided in the Visual Studio Code documentation: [Intro to Git - Publish local repository to GitHub](https://code.visualstudio.com/docs/sourcecontrol/intro-to-git#:~:text=Publish%20local%20repository%20to%20GitHub&text=Use%20the%20Publish%20to%20GitHub,code%20to%20the%20remote%20repository.)
+
+3. **GitHub Pages Deployment:**
+   - Deploying to GitHub Pages involved navigating to the Settings tab in the GitHub repository.
+   - From the source section drop-down menu, I selected the Main Branch.
+   - The Main branch selection triggered an automatic refresh, and a detailed ribbon display confirmed the successful deployment.
+
+### Live Content
+
+The live version of Indieverse is accessible
+
+## Credits
+
+### Code Creation
+
+This project was inspired by and used the code from [@vsafonova](https://github.com/vsafonova/memory-card-game) as a template. The code was adapted to fit the layout of my Final Fantasy Memory-Game. @vsafonova's creation was instrumental in my understanding of how to create a mini-game in JavaScript from scratch.
+
+### Websites and Resources Used
+
+The following websites and resources were used to assist in the creation of the webpage and mini-game, including sourcing images and conducting research to understand the steps taken, errors encountered, and trials conducted during the process:
+
+- [Learn Vanilla JavaScript Memory Game Tutorial](https://www.youtube.com/watch?v=eMhiMsEC9Uk&list=PLLX1I3KXZ-YH-woTgiCfONMya39-Ty8qw)
+- [Adding a countdown timer to the game](https://subscription.packtpub.com/book/game-development/9781849695466/1/ch01lvl1sec21/adding-a-countdown-timer-to-the-game)
+- [JavaScript Memory Game: Need to add congrats message at the end of game](https://stackoverflow.com/questions/54171904/javascript-memory-game-need-to-add-congrats-message-at-the-end-of-game)
+- [Adding time counter to my memory game script](https://stackoverflow.com/questions/42007631/adding-time-counter-to-my-memory-game-script)
+- [JavaScript Card Memory Game: Card shuffling function](https://stackoverflow.com/questions/47768146/javascript-card-memory-game-card-shuffling-function)
+- [How to randomize (shuffle) a JavaScript array](https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array)
+- [JavaScript Introduction - W3Schools](https://www.w3schools.com/js/js_intro.asp)
+- [2D breakout game using Phaser - MDN](https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_breakout_game_Phaser/Buttons)
+- [Codecademy Forum Discussion](https://www.codecademy.com/forum_questions/52d0c35b8c1ccc0daf0003e6)
+- [Quora Discussion on JavaScript Start and Stop Button]( https://www.quora.com/What-is-the-code-to-create-a-start-and-stop-button-using-JavaScript-on-a-web-page)
+- [Bootstrap Get Started - W3Schools](https://www.w3schools.com/bootstrap/bootstrap_get_started.asp)
+- [Finding Area Width - BU Tech](https://www.bu.edu/tech/services/cccs/websites/www/wordpress/how-to/find-area-width/)
+- [Adding a new project to GitHub using VS Code](https://stackoverflow.com/questions/46877667/how-to-add-a-new-project-to-github-using-vs-code)  
+
+### Images
+
+The images used in the game were sourced from the following locations:
+
+- [Front Face Images](https://www.reddit.com/r/FinalFantasy/comments/urj4k4/the_quest_to_make_icons_of_all_the_final_fantasy/)
+- [Back Face Image](https://images.app.goo.gl/hhEyD5vYZfScGRUm8)
+- [Start-up Box Image](https://www.desktopbackground.org/wallpaper/final-fantasy-vii-10th-anniversary-logo-by-raileysxerilyasrx-on-501796)
+
+### Sounds
+
+The sounds used in the game were sourced from the following locations:
+
+- [Winning Sound](https://soundboardguy.com/sounds/final-fantasy-7-victory-theme-mp3/#google_vignette)
+- [Game Over](https://pixabay.com/sound-effects/search/game-over/) sound were sourced from external websites.
+- [Flipping the cards, matching cards, running out of time, Success' sounds were imported from @vsafonova's GitHub](https://github.com/vsafonova/memory-card-game)
+
+### Favicon
+
+The favicon used was created and generated from the following location:
+
+- [Favicon](https://favicon.io/)
+
+## Validator
+
+### HTML
+
+![HTML](../validator-images/htmlvalidator.PNG)
+
+### CSS
+
+![CSS](../validator-images/cssvalidator.PNG)
+
+### JavaScript
+
+![JavaScript1](../validator-images/javascriptvalidator.PNG)
+![JavaScript2](../validator-images/javascriptvalidator2.PNG)
+
+## Lighthouse
+
+### Desktop
+
+![Cards Page Snapshot](../finalfantasy/validator-images/cardspagelighthouse.PNG)
+![Main Page Snapshot](../finalfantasy/validator-images/mainpagesnapshotlighthouse.PNG)
+![Main Page Navigation](../finalfantasy/validator-images/memorygamelighthouse.PNG)
+![TimeSpan](../finalfantasy/validator-images/timespanlighthouse.PNG)
+
+### Mobile
+
+![Main Page Navigation](../finalfantasy/validator-images/memorygamemobile.PNG)
+![Main Page Snapshot](../finalfantasy/validator-images/snapshotmobile.PNG)
+![Cards Page Snapshot](../finalfantasy/validator-images/cardssnapshotmobile.PNG)
+![Timespan](../finalfantasy/validator-images/timespanmobile.PNG)
---
+++ js/script.js
@@ -0,0 +1,307 @@
+//DOM Elements
+let gameContainerEl = document.querySelector("#gameContainer"); //Main game container
+let startContainerEl = document.querySelector("#startContainer"); // Start screen container
+let nameInputEl = document.querySelector("#nameInput"); // Inoput field for the player's name
+let startButtonEl = document.querySelector("#startButton"); // Start game button
+let formEl = document.querySelector("#form"); // Form for the player's name input
+let endScreenEl = document.querySelector("#endScreen"); // End screen container
+
+// Game Variables
+let userName = ""; // Player's name
+
+let gameWon = false; // Boolean to check if it is a won game
+let gameActive = false; // Boolean to check if the game is active
+
+let correctGuesses = 0; // Number of correct guesses
+let neededGuesses = 0; //Number of needed guesses to win the game
+
+// Time Variables
+const maxTime = 90; // Maximum time for the game
+const endTimeout = 10000; // Time before the end screen appears
+let timer = maxTime; // Current time
+let timerLow = new Audio("../sounds/TimeLow.mp3"); // Sound played when time is low
+
+//Card Variables
+let cardsArray = []; // Array to store the cards
+
+let scoreData = []; // Array to store the score data
+loadPlayerData(); // Function to load the player data
+
+//Card generator Function
+function createCards() {
+    //Generate the HTML for each card in the cardsArray
+    for (let i = 0; i < cardsArray.length; i++) {
+        let card = document.createElement("div");
+        let face = document.createElement("img");
+        let back = document.createElement("div");
+        card.classList = "card"; // Class for the card
+        face.classList = "face"; // Class for the face of the card
+        back.classList = "back"; // Class for the back of the card
+        //Attach the info to the cards
+        face.src = cardsArray[i].url; // Image URL for the card
+        card.setAttribute("name", cardsArray[i].name); // Name attribute for the card
+        //Attach the cards to the gameContainer
+        gameContainerEl.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+
+        //Event listener for the card click
+        card.addEventListener("click", function (e) {
+            toggleCard(card); // Function to toggle the card
+            compareCards(e); // Function to compare the cards
+        });
+    }
+}
+
+// Function to shuffle the cards
+function shuffleCards() {
+    cardsArray = cardsArray.concat(cardsArray); // Duplicate the cardsArray
+    cardsArray.sort(() => Math.random() - 0.5); // Shuffle the cardsArray
+}
+
+// Function to compare selected cards
+function compareCards(e) {
+    let clickedCard = e.target; // The card that was clicked
+    selectCard(clickedCard); // Function to select the clciked card
+
+    let selectedCards = document.querySelectorAll(".selected"); // All selected cards
+
+    //If there are not exaactly 2 selected cards, exit the function
+    if (selectedCards.length !== 2) {
+        return;
+    }
+
+    // If the two selected cards are equal
+    if (checkEqualCards(selectedCards[0], selectedCards[1])) {
+        playSuccessSound(); // Play sound indicating a successful match
+        winCondition(); // Check if the game is won
+        disablePointerEvents(selectedCards[0]); // Disable further interactions with the first card
+        disablePointerEvents(selectedCards[1]); // Disable further interactions with the second card
+    } else {
+        // If the cards are not equal, flip them back over after a delay
+        setTimeout(function () {
+            if (gameActive === false) {
+                return;
+            }
+            toggleCard(selectedCards[0]); // Flip the first card back over
+            toggleCard(selectedCards[1]); // Flip the second card back over
+            playUnFlipSound(); // Play a sound indicating the cards are being flipped over
+        }, 1000);
+    }
+
+    unselectCard(selectedCards[0]); // Unselected the first card
+    unselectCard(selectedCards[1]); // Unselected the second card
+}
+
+// Function to check if the game is won
+function winCondition() {
+    correctGuesses += 1; // Increment the number of correct guesses
+    if (correctGuesses >= neededGuesses) { // If the number of correct guesses is equal to or greater than the number of needed guesses
+        gameActive = false; // Set the game as inactive
+        storePlayerData(); // Store the player's data
+        endScreenEl.innerHTML = "Winner Winner Chicken Dinner!"; // Display a winning message
+        endScreenEl.classList.add("end-screen-shown"); // Show the end screen
+        playWinSound(); // Play sound inddicating the game is won
+        setTimeout(resetGame, endTimeout); // Reset the game after a delay
+    }
+}
+
+window.setInterval(gameTimer, 1000); // Set up a game timer that updates every second
+
+// Function to disable pointer events on a card
+function disablePointerEvents(card) {
+    card.style.pointerEvents = "none"; // Disable pointer events on the card
+}
+
+// Function to select a card
+function selectCard(card) {
+    card.classList.add("selected"); // Add the 'selected' class to the card
+    playFlipSound(); // Play a sound indicating the card is being flipped over
+}
+
+// Function to unselect a card
+function unselectCard(card) {
+    card.classList.remove("selected"); // Remove the 'selected' class from the card
+}
+
+// Function to toggle a card
+function toggleCard(card) {
+    card.classList.toggle("toggleCard"); // Toggle the 'toggleCard' class on the card
+}
+
+// Function to add the 'toggleCard' class to a card
+function addToggleCard(card) {
+    card.classList.add("toggleCard"); // Add the 'toggleCard' to the card
+}
+
+// Function to check if two cards are equal
+function checkEqualCards(card1, card2) {
+    return card1.getAttribute("name") === card2.getAttribute("name"); // Return true if the 'name' attribute of the two cards are equal
+}
+
+// Function for the game timer
+function gameTimer() {
+    if (gameActive === false) { // If the game is not active, exit the function
+        return;
+    }
+    timer -= 1; // Decrement the timer
+
+    let timerEl = document.querySelector("#gameTimer"); // The timer element
+    timerEl.innerHTML = "Timer: " + timeConvert(timer); // Update the timer display
+    // If the timer is low, add a class to the timer element and play a sound
+    if (timer > 12) {
+        timerEl.classList.remove("game-timer-low");
+    }
+    if (timer === 13) {
+        timerEl.classList.add("game-timer-low");
+        timerLow.play();
+    }
+
+    // If the timer reaches 0, end the game
+    if (timer <= 0) {
+        endGame();
+    }
+}
+
+// Function to end the game
+function endGame() {
+    gameActive = false; // Set the game as inactive
+    let cardEl = document.querySelectorAll(".card"); // All cards
+    for (let i = 0; i < cardEl.length; i++) {
+        addToggleCard(cardEl[i]); // Add the 'toggleCard' class to each card
+    }
+    endScreenEl.innerHTML = "Game Over!"; // Display a losing message
+    endScreenEl.classList.add("end-screen-shown"); // Show end screen
+    playLoseSound(); // Play a sound indicating the game is lost
+    setTimeout(resetGame, endTimeout); // Reset the game after a delay
+}
+
+// Function to convert time to minutes and seconds
+function timeConvert(timer) {
+    let minutes = Math.floor(timer / 60); // Calculate the minutes
+    let seconds = timer - minutes * 60; // Calculate the reamining seconds
+    if (seconds < 10) {
+        seconds = "0" + seconds; // Add a leading zero to seconds if less than 10
+    }
+    return minutes + ":" + seconds; // Return the time in the format 'minutes:seconds'
+}
+
+// Function to store the player's Data
+function storePlayerData() {
+    let timeUsed = maxTime - timer; // Calculate the time used
+    const maxScoreEntries = 10; // Maximum number of score entries
+    scoreData.push({ time: timeUsed, name: userName }); // Add the player's  data to the scoreData array
+    scoreData.sort((b, a) => b.time - a.time); // Sort the scoreData array by time
+    if (scoreData.length > maxScoreEntries) {
+        scoreData.length = maxScoreEntries; // Limit the number of score entries to the maximum
+    }
+    localStorage.setItem("scores", JSON.stringify(scoreData)); // Store the scoreData array in localStorage
+    createScoreBoard(); // Create the scoreBoard
+}
+
+// Function to load the player's data
+function loadPlayerData() {
+    scoreData = JSON.parse(localStorage.getItem("scores")) || []; // Load the scoreData array from localStorage
+    createScoreBoard(); // Create the scoreBoard
+}
+
+// Function to create the scoreBoard
+function createScoreBoard() {
+    clearScoreBoard(); // Clear the scoreBoard
+    let scoreBoardEl = document.querySelector("#scoreBoard"); // The scoreBoard element
+    for (let i = 0; i < scoreData.length; i++) {
+        let userScoreEl = document.createElement("div"); // Create a new score element
+        userScoreEl.classList.add("score"); // Add the 'score; class to the score element
+        userScoreEl.innerHTML =
+            i + 1 + ". " + scoreData[i].name + " " + timeConvert(scoreData[i].time); // Set the content of the score element
+        scoreBoardEl.append(userScoreEl); // Add the score element to the scoreBoard
+    }
+}
+
+// Function to clear the scoreBoard
+function clearScoreBoard() {
+    let scores = document.querySelectorAll(".score"); // All store elements
+    for (let i = 0; i < scores.length; i++) {
+        scores[i].remove(); // Remove each score element
+    }
+}
+
+// Async function to get data from an API
+async function getData() {
+let apiUrl = "../api/data.json"; // The API URL
+    try {
+        let response = await fetch(apiUrl); // Fetch the data from the API
+        let result = await response.json(); // Parse ethe data as JSON
+        cardsArray = result; // Set the cardsArray to the result
+        createDeck(); // Create the deck
+        neededGuesses = cardsArray.length; // Set the number of needed guesses to the lenght of the cardsArray
+        startGame(); // Start the game
+    } catch {
+        console.log("API error"); // Log an error message if there was an error fetching the data
+    }
+}
+
+// Function to reset the game
+function resetGame() {
+    endScreenEl.classList.remove("end-screen-shown"); // Hide the end screen
+
+    let timerEl = document.querySelector("#gameTimer"); // The timer element
+    timerEl.style.visibility = "hidden"; // Hide the timer
+    let card = document.querySelectorAll(".card"); // All cards
+    for (let i = 0; i < card.length; i++) {
+        card[i].remove(); // Remove each card
+    }
+    timer = maxTime; // Reset the timer
+    startContainerEl.classList.remove("start-container-hidden"); // Show the start container
+}
+
+// Function to start the game
+function startGame() {
+    gameActive = true; // Set the game as active
+    correctGuesses = 0; // Reset the number of correct guesses
+    shuffleCards(); // Shuffle the cards
+    createCards(); // Create the cards
+    let timerEl = document.querySelector("#gameTimer"); // The timer element
+    timerEl.style.visibility = "visible"; // SHow the timer
+}
+
+// Event listener for the form submission
+formEl.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the form from submitting
+    userName = nameInputEl.value; // Get the player's name from the input field
+    getData(); // Get the data for the game
+    startContainerEl.classList.add("start-container-hidden"); // Hide the start container
+});
+
+// Function to create the deck
+function createDeck() { 
+    cardsArray.sort(() => Math.random() - 0.5); // Shuffle the cardsArray
+    cardsArray = cardsArray.slice(-10); // Get the last 10 cards from the cardsArray
+}
+
+// Sounds effects
+function playFlipSound() {
+    let sound = new Audio("../sounds/Flip.mp3"); // The flip sound
+    sound.play(); // Play the flip sound
+}
+
+function playUnFlipSound() {
+    let sound = new Audio("../sounds/Unflip.mp3"); // The unflip sound
+    sound.play(); // Play the unflip sound
+}
+
+function playWinSound() { 
+    timerLow.pause(); // Pause the low timer sound
+    let sound = new Audio("../sounds/finalfantasyvictory.mp3"); // The win sound
+    sound.play(); // Play the win sound
+}
+
+function playLoseSound() {
+    let sound = new Audio("../sounds/demongameover.mp3"); // The lose sound
+    sound.play(); // Play the lose sound
+}
+
+function playSuccessSound() {
+    let sound = new Audio("../sounds/Success.mp3"); // The success sound
+    sound.play(); // Play the success sound
+}
